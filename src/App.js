e65911149f7bf0458485a47d417c329556e3be8f
Martin <martingauna2000@gmail.com>
2021-12-03 23:00:32 -0300

    fix links

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <div className="navbar">
         <img src={img} alt="" className="logo"/>
         <ul>
-          <li><a href="#">Lorem</a></li>
-          <li><a href="#">Lorem</a></li>
-          <li><a href="#">Lorem</a></li>
-          <li><a href="#">Lorem</a></li>
+          <li><a href="https://www.instagram.com/martin_gauna23/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
+          <li><a href="https://www.facebook.com/tino.gauna.10" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>
+          <li><a href="https://twitter.com/Martngauna5" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/martin-gauna-009a68208/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></li>
         </ul>
       </div>
 
